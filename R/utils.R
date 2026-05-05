49e3d9c9f6a623f266ab7e3f0d5b7c90e28cb100
Jan Vanhove <janvanhove@gmail.com>
2026-05-05 12:41:07 +0200

    Normalizing function

--- R/utils.R
+++ R/utils.R
@@ -31,6 +31,8 @@ dot <- function(a, b) {
 }
 #' Compute squared norms
 #'
+#' Computes the squared Euclidean norm(s) of one or more vectors.
+#'
 #' @param a A numeric vector or matrix.
 #'
 #' @return The squared Euclidean norm of `a` (if `a` is a vector)
@@ -44,4 +46,36 @@ dot <- function(a, b) {
 squared_norm <- function(a) {
   if (is.vector(a)) return(dot(a, a))
   apply(a, MARGIN = 1, squared_norm)
+}
+#' Compute norms
+#'
+#' Computes the Euclidean norm(s) of one or more vectors.
+#'
+#' @param a A numeric vector or matrix.
+#'
+#' @return The Euclidean norm of `a` (if `a` is a vector)
+#'    or the Euclidean norms of each row of `a` (if `a` is a matrix).
+#'
+#' @examples
+norm <- function(a) {
+  sqrt(squared_norm(a))
+}
+#' Normalize vectors to unit length
+#'
+#' @param a A numeric vector or matrix.
+#'
+#' @return `a` scaled to have unit norm (if `a` is a vector) or
+#'    `a` with each row scaled to have unit length (if `a` is a matrix).
+#' @export
+#' x <- c(1, 2, 3)
+#' normalize(x)
+#' m <- matrix(rnorm(40), nrow = 10)
+#'
+#' @examples
+normalize <- function(a) {
+  if (is.vector(a)) {
+    if (isTRUE(all.equal(norm(a), 0))) return(NaN)
+    return(a / norm(a))
+  }
+  apply(a, MARGIN = 1, normalize) |> t()
 }
